d1941d845fb8d34b869b9231c5784b22f37a0875
WELCE <3245185172@qq.com>
2021-12-16 11:29:49 +0800

    unity

---
+++ Frist game/Assets/test/Car.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public GameObject flash;
+    public Collider2D coll;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Player")
+        {
+            flash.SetActive(true);
+            //coll.enabled = false;
+        }
+    }
+}
